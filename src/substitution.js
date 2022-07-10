319899b26d3afb3be88b664c75d4fb74ab3f8f8b
Erick M <erick.michelr@gmail.com>
2022-07-10 17:48:58 -0500

    Almost done with substitution

--- src/substitution.js
+++ src/substitution.js
@@ -8,7 +8,100 @@ const substitutionModule = (function () {
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    if(!alphabet || alphabet.length != 26 || !checkIfUnique(alphabet)){
+      return false
+    }
+    alphabetSplit = alphabet.split("")
+   
+    if(encode == true){
+      return getEncoded(input)
+
+    }
+    else if(encode ==false){
+      return getDecoded(input)
+    }
   }
+
+  //Function to create commonAlphabet array
+  const commonAlphabet = () =>{
+    const commonAlphabetAr = []
+    for(let i = 0; i < 26; i++){
+      commonAlphabetAr.push(String.fromCharCode(97+i)) 
+    }
+    return commonAlphabetAr
+  }
+
+  //Function to Decode Message
+  const getDecoded = (messageToDecode) => {
+    let messageToDecodeSplit = messageToDecode.split("")
+    let commonAlphabetAr = commonAlphabet();
+    let newLetter =""
+
+    messageToDecodeSplit.forEach((letter)=>{
+      if(letter == " "){
+        newLetter+=" "
+      }
+      else{
+        commonAlphabetAr.find((element)=>{
+          if(letter == element){
+            let commonIndex = commonAlphabetAr.indexOf(element)
+            newLetter += alphabetSplit.at(commonIndex)
+            // console.log(newLetter)
+          }
+        })
+      }
+    })
+    
+    return newLetter
+  }
+  
+
+  //Function to Encode Message
+  const getEncoded = (messageToEncode) =>{
+    let messageToEncodeSplit = messageToEncode.split("")
+    let commonAlphabetAr = commonAlphabet();
+    let newLetter =""
+
+    messageToEncodeSplit.forEach((letter)=>{
+      if(letter == " "){
+        newLetter+=" "
+      }
+      else{
+        commonAlphabetAr.find((element)=>{
+          if(letter == element){
+            let commonIndex = commonAlphabetAr.indexOf(element)
+            newLetter += alphabetSplit.at(commonIndex)
+            // console.log(newLetter)
+          }
+        })
+      }
+    })
+    
+    return newLetter
+  }
+
+  //Function check if alphabet contains unique chars
+   function checkIfUnique(chars){
+    let splitStr = chars.split("")
+    let splitStrCopy = chars.split("")
+    let duplicatedCharFlag = false
+    let unique = true
+
+    splitStr.forEach((element)=> {
+      splitStrCopy.splice(0, 1)
+      duplicatedCharFlag = splitStrCopy.some((duplicated) => element == duplicated)
+
+      if(duplicatedCharFlag == true){
+        unique = false
+      }  
+
+      
+
+    })
+    return unique
+   }
+   console.log("ykrrpik")
+   console.log(substitution("message", ".lmoknijbuhvygctfxrdzeswaq"))
 
   return {
     substitution,
